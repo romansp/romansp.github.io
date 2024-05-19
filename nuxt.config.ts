9c69f595fdaf2fdb2f88e5fa984173f55100ee30
Raman Paulau <rspaulau@gmail.com>
2024-05-19 18:13:12 -0500

    configure eslint, and save auto-fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,15 +7,11 @@ export default defineNuxtConfig({
   ],
   content: {
     highlight: {
-      // OR
       theme: {
-        // Default theme (same as single string)
-        default: 'github-light',
-        // Theme used if `html.dark`
-        dark: 'github-dark',
-        // Theme used if `html.sepia`
-        sepia: 'monokai'
-      }
+        default: 'vitesse-dark',
+        dark: 'vitesse-dark',
+        light: 'vitesse-light',
+      },
     }
 
   }

0ddc6faae2d2fffb603b1355b914c854147de716
Raman Paulau <rspaulau@gmail.com>
2024-05-26 11:02:27 -0700

    fix build

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,17 +1,40 @@
 <script setup lang="ts">
 import { format } from 'date-fns'
+
+const route = useRoute()
+const { data } = await useAsyncData(route.fullPath, () => queryContent(route.fullPath).findOne())
+
+useSeoMeta({
+  twitterTitle: computed(() => data.value?.title),
+  twitterDescription: computed(() => data.value?.description),
+  twitterImage: 'https://paulau.dev/og-image.png',
+  twitterCard: 'summary_large_image',
+})
 </script>
 
 <template>
   <main>
-    <ContentDoc v-slot="{ doc }">
-      <article class="prose dark:prose-invert">
-        <h1>{{ doc.title }}</h1>
-        <p>
-          <time :datetime="doc.date">{{ format(doc.date, "PPP") }}</time>
-        </p>
-        <ContentRenderer :value="doc" />
-      </article>
+    <ContentDoc>
+      <template #default="{ doc }">
+        <article class="prose dark:prose-invert">
+          <h1>{{ doc.title }}</h1>
+          <p>
+            <time :datetime="doc.date">{{ format(doc.date, "PPP") }}</time>
+          </p>
+          <ContentRenderer :value="doc" />
+        </article>
+      </template>
+      <template #not-found>
+        <div class="prose dark:prose-invert">
+          <h1>404</h1>
+          <p>
+            Page Not Found
+          </p>
+          <NuxtLink to="/">
+            Home
+          </NuxtLink>
+        </div>
+      </template>
     </ContentDoc>
   </main>
 </template>

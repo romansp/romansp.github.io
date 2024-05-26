8357aa15a5149510bf3853f58b6f8fd780706682
Raman Paulau <rspaulau@gmail.com>
2024-05-26 03:13:51 -0700

    center content

--- app.vue
+++ app.vue
@@ -31,7 +31,7 @@ useSeoMeta({
       </nav>
     </header>
     <div class="px-4 py-10 max-w-4xl mx-auto sm:px-6 sm:py-12 lg:max-w-4xl lg:py-16 lg:px-8 xl:max-w-6xl">
-      <NuxtPage class="mx-auto" />
+      <NuxtPage class="mx-auto flex justify-center" />
     </div>
   </div>
 </template>

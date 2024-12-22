e422bd3f2c7ce064516fcf2464bce2c854289018
Raman Paulau <rspaulau@gmail.com>
2024-12-22 10:57:44 -0800

    force trailing slash, enable canonical links, set site name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,11 +10,13 @@ export default defineNuxtConfig({
     'nuxt-og-image',
     '@nuxtjs/sitemap',
     '@nuxtjs/robots',
+    'nuxt-seo-utils',
   ],
 
   site: {
     url: 'https://paulau.dev',
-    name: 'paulau.dev',
+    name: 'Raman Paulau',
+    trailingSlash: true,
   },
 
   content: {
@@ -28,4 +30,21 @@ export default defineNuxtConfig({
   },
 
   compatibilityDate: '2024-11-28',
+
+  experimental: {
+    defaults: {
+      nuxtLink: {
+        trailingSlash: 'append',
+      },
+    },
+  },
+
+  hooks: {
+    // Ensure page trailing slash
+    'pages:extend': function (pages) {
+      for (const page of pages) {
+        page.path = page.path.endsWith('/') ? page.path : `${page.path}/`
+      }
+    },
+  },
 })

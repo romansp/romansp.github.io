fb99c5d1aa92b86fdd8a59a37e3f603e1ed7583e
Raman Paulau <rspaulau@gmail.com>
2025-01-01 21:39:16 -0800

    add publication and modification timestamps to blog posts

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -4,9 +4,18 @@ import { format } from "date-fns";
 const route = useRoute();
 const { data } = await useAsyncData(route.fullPath, () => queryContent(route.fullPath).findOne());
 
+function toDate(date: string) {
+  if (!date) {
+    return;
+  }
+  return new Date(date).toISOString();
+}
+
 useSeoMeta({
   twitterTitle: computed(() => data.value?.title),
   twitterDescription: computed(() => data.value?.description),
+  articlePublishedTime: computed(() => toDate(data.value?.date)),
+  articleModifiedTime: computed(() => toDate(data.value?.updatedAt ?? data.value?.date)),
 });
 
 defineOgImageComponent("NuxtSeo");
@@ -17,9 +26,14 @@ defineOgImageComponent("NuxtSeo");
     <ContentDoc>
       <template #default="{ doc }">
         <article class="prose dark:prose-invert">
-          <h1>{{ doc.title }}</h1>
-          <p class="opacity-70 text-xs -mt-6">
+          <h1 class="mb-0">
+            {{ doc.title }}
+          </h1>
+          <p class="opacity-70 text-xs">
             <time :datetime="doc.date">{{ format(doc.date, "PPP") }}</time>
+          </p>
+          <p v-if="doc.updatedAt" class="opacity-70 text-xs">
+            (updated: <time :datetime="doc.updatedAt">{{ format(doc.updatedAt, "PPP") }}</time>)
           </p>
           <ContentRenderer :value="doc" />
           <a :href="`https://github.com/romansp/romansp.github.io/blob/main/content${doc._path}/index.md`">Suggest edit to this page</a>
